1cda058c4e6a854f1472df0d87094dab63de63a9
Darius <bakunas@gmail.com>
2023-04-30 17:07:56 -0400

    chore: init arduino firmware project

---
+++ arduino-firmware/arduino-firmware.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		arduino_firmware.ino
+ Created:	4/30/2023 5:03:11 PM
+ Author:	bakun
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
