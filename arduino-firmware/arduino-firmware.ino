6d5894986ea92f485659a5eefe4279f1dd7860ab
Darius <bakunas@gmail.com>
2023-04-30 17:54:19 -0400

    feat: initial arduino open/close cmds

--- arduino-firmware/arduino-firmware.ino
+++ arduino-firmware/arduino-firmware.ino
@@ -4,12 +4,50 @@
  Author:	bakun
 */
 
+#include <Servo.h>
+
+Servo opener;
+
+constexpr auto SEPARATOR = '#';
+constexpr auto CMD_OPEN = "OPEN";
+constexpr auto CMD_CLOSE = "CLOSE";
+constexpr auto CMD_IS_OPEN = "IS_OPEN";
+constexpr auto POS_CLOSED = 0;
+constexpr auto POS_OPEN = 180;
+auto open = false;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	Serial.begin(57600);
+	Serial.flush();
+	opener.attach(2);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	String cmd;
+
+	if (Serial.available() > 0) {
+		cmd = Serial.readStringUntil(SEPARATOR);
+
+		if (cmd == CMD_IS_OPEN) {
+			if (open) {
+				Serial.print("OPEN");
+			}
+			else {
+				Serial.print("CLOSED");
+			}
+			Serial.println(SEPARATOR);
+		}
+		else if (cmd == CMD_CLOSE) {
+			opener.write(POS_CLOSED);
+			open = false;
+			Serial.println(SEPARATOR);
+		}
+		else if (cmd == CMD_OPEN) {
+			opener.write(POS_OPEN);
+			open = true;
+			Serial.println(SEPARATOR);
+		}		
+	}
 }
